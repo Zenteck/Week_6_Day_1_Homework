1371a46a3c10dfa818f515f9bb7123843dddd15c
Alan <spambox+github@zenteck.co.uk>
2019-12-10 09:08:02 +0000

    homework

---
+++ whodunnit.js
@@ -0,0 +1,210 @@
+Episode 1
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+//This should return the name Miss Scarlet.
+//Correct
+
+Episode 2
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+// This will throw an error. Murderer was defined in the first line, redefining it is not possible.
+//Correct
+
+Episode 3
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+// This will return Miss Peacock first, as that let murderer is within the scope of the called function.
+// It will then throw an error, as the let murderer == Professor Plum cannot be referenced.
+// Got second part wrong
+
+Episode 4
+
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+//First line will list the three suspects, with Mustard third.
+//Suspect three will be Colonel Mustard. Suspect three was redefined within the scope of the called function declareAllSuspects.
+//Second part wrong. Oh, the let = Mustard is only in that local scope.
+
+
+
+Episode 5
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+//This will still return the candlestick. The change would not apply to all that followed.
+//Nope. Revolver. So changing something within a constant.
+
+Episode 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+//This will return Colnel Mustard, but probably wrong.
+//Nope. I don't fully understand why.
+
+
+
+Episode 7
+
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+//I think this will be Mr Green, as the let for plotTwist cannot be accessed above this functuion.
+//Still not sure why oit wasnt miss Scarlet
+
+Episode 8
+
+const scenario = {
+  murderer: 'Mrs. Peacock',1
+  room: 'Conservatory',1
+  weapon: 'Lead Pipe'1
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+//This will be the candlestick. The top line of plottwist run before unexpectedOutcome is ever called. Then when it does, the murderer is Mustard.
+//Hallejuah!!!
+
+Episode 9
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+//Stays as Professor Plum. The let in the if statement shouldn't affect the later function.
+//At least I've ended on a good one.
+
+Extensions
+
+Make up your own episode!
